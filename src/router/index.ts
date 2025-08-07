44296f2eee16e0eaf164d4ba3023febd5ac3d9e0
magley <60552482+magley@users.noreply.github.com>
2025-08-07 19:39:00 +0200

    refactor: Add `User` prefix to component names
    "Settings" is very generic. This will give us problems later, with importing components in the router file.

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,15 +2,15 @@
 import 'vue-router';
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/Home.vue';
-import Login from '../views/user/Login.vue';
-import Register from '../views/user/Register.vue';
-import Settings from '../views/user/Settings.vue';
+import UserLogin from '../views/user/UserLogin.vue';
+import UserRegister from '../views/user/UserRegister.vue';
+import UserSettings from '../views/user/UserSettings.vue';
 
 const routes = [
     { path: '/', component: Home },
-    { path: '/login', component: Login },
-    { path: '/register', component: Register },
-    { path: '/update', component: Settings },
+    { path: '/login', component: UserLogin },
+    { path: '/register', component: UserRegister },
+    { path: '/update', component: UserSettings },
 ];
 
 export const router = createRouter({
