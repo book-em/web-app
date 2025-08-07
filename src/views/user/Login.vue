69ae3517e9087faff2812ab738c70e54926dec07
magley <60552482+magley@users.noreply.github.com>
2025-08-07 15:57:50 +0200

    feat: Add login page

--- src/views/user/Login.vue
+++ src/views/user/Login.vue
@@ -1,3 +1,66 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { AuthAPI, type LoginRequestDTO, type LoginResponseDTO } from '../../api/auth.api';
+import type { AxiosError, AxiosResponse } from 'axios';
+import { setJWT } from '../../api/localstorage';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+const formUsernameOrEmail = ref('');
+const formPassword = ref('');
+const error = ref('');
+
+const doLogin = () => {
+    const usernameOrEmail = formUsernameOrEmail.value;
+    const password = formPassword.value;
+    const dto: LoginRequestDTO = {
+        usernameOrEmail,
+        password
+    };
+
+    error.value = "";
+
+    AuthAPI.login(dto).then((res: AxiosResponse<LoginResponseDTO>) => {
+        const jwt = res.data.jwt;
+        setJWT(jwt);
+        router.push('/');
+    }).catch((err: AxiosError) => {
+        if (err.response?.status == 400) {
+            error.value = "Unknown user or password";
+        } else {
+            console.error(err);
+            error.value = "Unknown error. Check the console";
+        }
+    });
+}
+
+</script>
+
 <template>
-Login
+    <p v-if="error.length > 0" class="error">
+        {{ error }}
+    </p>
+
+    Login
+    <form @submit.prevent="doLogin">
+        <label>
+            <input v-model.trim="formUsernameOrEmail" type="text" />
+            Username or email
+        </label>
+        <br />
+
+        <label>
+            <input v-model.trim="formPassword" type="password" />
+            Password
+        </label>
+        <br />
+
+        <button type="submit">Log In</button>
+    </form>
 </template>
+
+<style lang="css" scoped>
+.error {
+    color: red;
+}
+</style>
