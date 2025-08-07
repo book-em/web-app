f3307587f55dde99328b759a2d9298a79ba6a1fc
magley <60552482+magley@users.noreply.github.com>
2025-08-07 15:57:50 +0200

    feat: Add login page

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -5,9 +5,15 @@
 <template>
     <nav class="navbar">
         <ul>
-            <li><router-link to="/"><strong>Book'em</strong></router-link></li>
-            <li><router-link to="/login">Login</router-link></li>
-            <li><router-link to="/register">Register</router-link></li>
+            <li>
+                <RouterLink to="/"><strong>Book'em</strong></RouterLink>
+            </li>
+            <li>
+                <RouterLink to="/login">Login</RouterLink>
+            </li>
+            <li>
+                <RouterLink to="/register">Register</RouterLink>
+            </li>
         </ul>
     </nav>
 </template>
