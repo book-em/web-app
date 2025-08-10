c5e600b4bf71cbc85eb0fb1b4bfc770bf6aa263a
magley <60552482+magley@users.noreply.github.com>
2025-08-10 15:01:01 +0200

    feat: Add styling to register

--- src/views/user/UserRegister.vue
+++ src/views/user/UserRegister.vue
@@ -6,6 +6,7 @@ import { setJWT } from '../../api/localstorage';
 import { UserRole, type UserDTO } from '../../api/user.api';
 import { useRouter } from 'vue-router';
 import { useAuthStore } from '../../stores/auth-store';
+import { Form } from '@primevue/forms';
 
 const auth = useAuthStore();
 const router = useRouter();
@@ -20,6 +21,13 @@ const formDTO = ref<RegisterRequestDTO>({
     role: UserRole.Guest,
 });
 const error = ref('');
+const loading = ref(false);
+
+const rolesAvailable = [
+    { label: 'Guest', value: UserRole.Guest },
+    { label: 'Host', value: UserRole.Host },
+    { label: 'Admin', value: UserRole.Admin }
+];
 
 onMounted(() => {
     auth.checkLocalStorage();
@@ -48,6 +56,7 @@ const doRegister = () => {
         return;
     }
     error.value = "";
+    loading.value = true;
 
     AuthAPI.register(dto).then((_: AxiosResponse<UserDTO>) => {
         loginAsNewlyCreatedUser(dto.username, dto.password);
@@ -61,7 +70,9 @@ const doRegister = () => {
             console.error(err);
             error.value = "Unknown error. Check the console";
         }
-    });
+    }).finally(() => {
+        loading.value = false;
+    });;
 }
 
 const loginAsNewlyCreatedUser = (username: string, password: string) => {
@@ -78,34 +89,111 @@ const loginAsNewlyCreatedUser = (username: string, password: string) => {
 </script>
 
 <template>
-    <p v-if="error.length > 0" class="error">
-        {{ error }}
-    </p>
+    <div class="login-background">
+        <Card class="center">
+            <template #title>
+                <h2>Create an account</h2>
+            </template>
+            <template #content>
+                <Form @submit="doRegister">
+                    <div class="form-div">
+                        <Fieldset legend="Account info">
+                            <InputText type="text" placeholder="Username" v-model.trim="formDTO!.username" fluid />
+                            <InputText type="email" placeholder="user@email.com" v-model.trim="formDTO!.email" fluid />
+                            <InputText type="password" placeholder="Password" v-model.trim="formDTO!.password" fluid />
+                            <InputText type="password" placeholder="Confirm password" v-model.trim="formConfirmPassword"
+                                fluid />
+                            <Select v-model="formDTO!.role" :options="rolesAvailable" optionLabel="label"
+                                optionValue="value" placeholder="Select a role" />
+                        </Fieldset>
 
-    Register
-    <form @submit.prevent="doRegister">
-        <label> <input v-model.trim="formDTO!.username" type="text" /> Username</label><br />
-        <label> <input v-model.trim="formDTO!.email" type="email" /> Email</label><br />
-        <label> <input v-model.trim="formDTO!.password" type="password" /> Password</label><br />
-        <label> <input v-model.trim="formConfirmPassword" type="password" /> Confirm Password</label><br />
-        <label> <input v-model.trim="formDTO!.name" type="text" /> Name</label><br />
-        <label> <input v-model.trim="formDTO!.surname" type="text" /> Surname</label><br />
-        <label> <input v-model.trim="formDTO!.address" type="text" /> Address</label><br />
-        <label>
-            <select v-model="formDTO!.role">
-                <option disabled value="">Please select one</option>
-                <option>{{ UserRole.Host }}</option>
-                <option>{{ UserRole.Guest }}</option>
-                <option>{{ UserRole.Admin }}</option>
-            </select> Role
-        </label><br />
 
-        <button type="submit">Register</button>
-    </form>
+                        <Fieldset legend="Personal info">
+                            <div class="inline-fields">
+                                <InputText type="text" placeholder="Name" v-model.trim="formDTO!.name" fluid />
+                                <InputText type="text" placeholder="Surname" v-model.trim="formDTO!.surname" fluid />
+                            </div>
+                            <InputText type="text" placeholder="Address" v-model.trim="formDTO!.address" fluid />
+                        </Fieldset>
+
+
+                        <Button class="btn" type="submit" severity="success" :disabled="loading">
+                            Register
+                        </Button>
+
+                        <Message v-show="error.length > 0" severity="error" size="small" variant="simple">{{ error }}
+                        </Message>
+                    </div>
+                </Form>
+            </template>
+            <template #footer>
+                <RouterLink class="text-center mt" to="/login">Or sign in</RouterLink>
+            </template>
+        </Card>
+    </div>
 </template>
 
 <style lang="css" scoped>
-.error {
-    color: red;
+.login-background {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    background-image: url('bg-houses.jpg');
+    background-position: center;
+    z-index: -100;
+    background-position: 8px -22px;
+}
+
+.center {
+    margin: auto;
+    width: 40%;
+    background-color: rgba(255, 255, 255, 0.85);
+    margin-top: 2em;
+    padding: 0em 2em;
+    border-radius: 10px;
+    box-shadow: 16px 32px 32px rgba(0, 0, 0, 0.4);
+}
+
+h2 {
+    text-align: center;
+    font-size: 2em;
+}
+
+.text-center {
+    display: block;
+    text-align: center;
+}
+
+.form-div {
+    margin-top: 1em;
+}
+
+.btn-spinner {
+    width: 1em;
+    height: 1em;
+    margin-left: 0.5em;
+}
+
+.form-div * {
+    margin-top: 0.25em;
+}
+
+.form-div .btn {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    margin-top: 1em;
+    width: 32%;
+}
+
+.inline-fields {
+    display: flex;
+    gap: 1em;
+}
+
+.inline-fields>* {
+    flex: 1;
 }
 </style>
