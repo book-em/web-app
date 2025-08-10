2eec09d6afe46143ef91b76550eaa1c49a13a6a3
magley <60552482+magley@users.noreply.github.com>
2025-08-10 13:52:52 +0200

    feat: Add styling to the login page

--- src/views/user/UserLogin.vue
+++ src/views/user/UserLogin.vue
@@ -2,15 +2,18 @@
 import { onMounted, ref } from 'vue';
 import { AuthAPI, type LoginRequestDTO, type LoginResponseDTO } from '../../api/auth.api';
 import type { AxiosError, AxiosResponse } from 'axios';
-import { getJWTId, getJWTRole, getJWTUsername, setJWT } from '../../api/localstorage';
 import { useRouter } from 'vue-router';
 import { useAuthStore } from '../../stores/auth-store';
+import { Form } from '@primevue/forms';
 
 const auth = useAuthStore();
 const router = useRouter();
 const formUsernameOrEmail = ref('');
 const formPassword = ref('');
 const error = ref('');
+const loading = ref(false);
+
+
 
 onMounted(() => {
     auth.checkLocalStorage();
@@ -25,6 +28,7 @@ const doLogin = () => {
     };
 
     error.value = "";
+    loading.value = true;
 
     AuthAPI.login(dto).then((res: AxiosResponse<LoginResponseDTO>) => {
         const jwt = res.data.jwt;
@@ -37,36 +41,91 @@ const doLogin = () => {
             console.error(err);
             error.value = "Unknown error. Check the console";
         }
+    }).finally(() => {
+        loading.value = false;
     });
 }
 
 </script>
 
 <template>
-    <p v-if="error.length > 0" class="error">
-        {{ error }}
-    </p>
-
-    Login
-    <form @submit.prevent="doLogin">
-        <label>
-            <input v-model.trim="formUsernameOrEmail" type="text" />
-            Username or email
-        </label>
-        <br />
-
-        <label>
-            <input v-model.trim="formPassword" type="password" />
-            Password
-        </label>
-        <br />
-
-        <button type="submit">Log In</button>
-    </form>
+    <div class="login-background">
+    <Card class="center">
+        <template #title><h2>Login</h2></template>
+        <template #content>
+            <Form @submit="doLogin" >    
+                <div class="form-div" >
+                    <InputText type="text" placeholder="Username or email" v-model.trim="formUsernameOrEmail" fluid />
+                    <InputText type="password" placeholder="Password"  v-model.trim="formPassword" fluid />
+                    
+                    <Button class="btn" type="submit" severity="success" :disabled="loading">
+                        Log in 
+                        <ProgressSpinner v-show="loading" class="btn-spinner" strokeWidth="8" />
+                    </Button>
+                    
+                    <Message v-show="error.length > 0" severity="error" size="small" variant="simple">{{ error }}</Message>
+                </div>
+            </Form>
+        </template>
+        <template #footer>
+            <RouterLink class="text-center mt" to="/register">Or create an account</RouterLink>
+        </template>
+    </Card>
+    </div>
 </template>
 
 <style lang="css" scoped>
-.error {
-    color: red;
+
+.login-background {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    background-image: url('bg-houses.jpg');
+    background-position: center;
+    z-index: -100;
+    background-position: 8px -22px;
+}
+
+.center {
+    margin: auto;
+    width: 40%;
+    margin-top: 8em;
+    background-color: rgba(255, 255, 255, 0.85); /* Optional frosted glass */
+    padding: 2em;
+    border-radius: 10px;
+    box-shadow: 16px 32px 32px rgba(0,0,0,0.4);
+} 
+
+h2 {
+    text-align: center;
+    font-size: 2em;
+}
+
+.text-center {
+    display:block;
+    text-align: center;
+}
+
+.form-div {
+    margin-top: 1em;
+}
+
+.btn-spinner {
+    width: 1em;
+    height: 1em;
+    margin-left: 0.5em;
+}
+
+.form-div > * {
+    margin-top: 1em;
+}
+
+.form-div .btn {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    width: 32%;
 }
 </style>
