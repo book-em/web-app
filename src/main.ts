5c166a5ab179436d5e3216a6010d8f66e10b669d
magley <60552482+magley@users.noreply.github.com>
2025-08-10 13:52:52 +0200

    feat: Add styling to the login page

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,35 @@ import './style.css';
 import App from './App.vue';
 import { router } from './router';
 import { createPinia } from 'pinia';
+import PrimeVue from 'primevue/config';
+import Aura from '@primeuix/themes/aura';
+import InputText from 'primevue/inputtext';
+import Button from 'primevue/button';
+import Message from 'primevue/message';
+import { ProgressSpinner } from 'primevue';
+import Card from 'primevue/card';
 
 const app = createApp(App);
 
 app.use(router);
 app.use(createPinia());
+
+app.use(PrimeVue, {
+    ripple: true,
+    theme: {
+        preset: Aura,
+        options: {
+            prefix: 'p',
+            darkModeSelector: 'system',
+            cssLayer: false
+        }
+    }
+});
+app.component('InputText', InputText);
+app.component('Button', Button);
+app.component('Message', Message);
+app.component('ProgressSpinner', ProgressSpinner);
+app.component('Card', Card);
+
 
 app.mount('#app');
