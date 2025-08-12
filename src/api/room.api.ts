5025d4823c375db48520851823ef4a565140f0e9
magley <60552482+magley@users.noreply.github.com>
2025-08-12 11:24:16 +0200

    feat: Create API client for `find room by id` and `create room`
    At this point in time, we don't have an API gateway. So I created another axios instance, just for the room microservice, for now, since it's quicker. I'll fix it later.

---
+++ src/api/room.api.ts
@@ -0,0 +1,35 @@
+import type { AxiosResponse } from "axios"
+import { axiosInstanceRooms } from "./util"
+
+export interface RoomDTO {
+    id: number,
+    hostID: number,
+    name: string,
+    description: string,
+    address: string
+    minGuests: number,
+    maxGuests: number,
+    photos: string[],
+    commodities: string[]
+}
+
+export interface RoomCreateDTO {
+    hostID: number,
+    name: string,
+    description: string,
+    address: string
+    minGuests: number,
+    maxGuests: number,
+    photosPayload: string[],
+    commodities: string[]
+}
+
+export class RoomAPI {
+    static create(dto: RoomCreateDTO): Promise<AxiosResponse<RoomDTO>> {
+        return axiosInstanceRooms.post(`/new`, dto);
+    }
+
+    static findById(id: number): Promise<AxiosResponse<RoomDTO>> {
+        return axiosInstanceRooms.get(`/${id}`);
+    }
+}
