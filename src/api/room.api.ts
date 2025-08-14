61f69ea9560aa28492fbc8ec643fcc753d0d000b
magley <60552482+magley@users.noreply.github.com>
2025-08-14 19:25:26 +0200

    feat: Create API for room availability

--- src/api/room.api.ts
+++ src/api/room.api.ts
@@ -24,6 +24,46 @@ export interface RoomCreateDTO {
     commodities: string[],
 }
 
+export interface CreateRoomAvailabilityListDTO {
+    roomId: number,
+    items: CreateRoomAvailabilityItemDTO[],
+}
+
+export interface RoomAvailabilityListDTO {
+    id: number,
+    roomId: number,
+    /**
+     * Date object
+     */
+    effectiveFrom: string,
+    items: RoomAvailabilityItemDTO[],
+}
+
+export interface CreateRoomAvailabilityItemDTO {
+    /**
+     * Date object
+     */
+    dateFrom: string,
+    /**
+     * Date object
+     */
+    dateTo: string,
+    available: boolean
+}
+
+export interface RoomAvailabilityItemDTO {
+    id: number,
+    /**
+     * Date object
+     */
+    dateFrom: string,
+    /**
+     * Date object
+     */
+    dateTo: string,
+    available: boolean
+}
+
 export class RoomAPI {
     static create(dto: RoomCreateDTO): Promise<AxiosResponse<RoomDTO>> {
         return axiosInstanceRooms.post(`/new`, dto);
@@ -35,5 +75,21 @@ export class RoomAPI {
 
     static findByHostId(hostId: number): Promise<AxiosResponse<RoomDTO[]>> {
         return axiosInstanceRooms.get(`/host/${hostId}`);
+    }
+
+    static findCurrentAvailabilityListOfRoom(roomId: number): Promise<AxiosResponse<RoomAvailabilityListDTO>> {
+        return axiosInstanceRooms.get(`/available/room/${roomId}`);
+    }
+
+    static findAvailabilityListsByRoomId(roomId: number): Promise<AxiosResponse<RoomAvailabilityListDTO[]>> {
+        return axiosInstanceRooms.get(`/available/room/all/${roomId}`);
+    }
+
+    static findAvailabilityListById(id: number): Promise<AxiosResponse<RoomAvailabilityListDTO>> {
+        return axiosInstanceRooms.get(`/available//${id}`);
+    }
+
+    static updateAvailability(dto: CreateRoomAvailabilityListDTO): Promise<AxiosResponse<RoomAvailabilityListDTO>> {
+        return axiosInstanceRooms.post(`/available`, dto);
     }
 }
