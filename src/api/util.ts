c7cc25bc0f7d12390ffd6f8502d1767512e00603
magley <60552482+magley@users.noreply.github.com>
2025-08-31 17:02:52 +0200

    feat: Create (temporary) axios instance for reservation API

--- src/api/util.ts
+++ src/api/util.ts
@@ -3,7 +3,6 @@ import axios from "axios";
 export const axiosInstance = axios.create({
     baseURL: "http://localhost:8500/api",
 });
-
 axiosInstance.interceptors.request.use(
     config => {
         const token = localStorage.getItem('jwt');
@@ -14,18 +13,15 @@ axiosInstance.interceptors.request.use(
     },
     error => Promise.reject(error)
 );
-
 axiosInstance.interceptors.response.use(
     response => response,
     error => Promise.reject(error)
 );
 
-
 // TODO: Instead of two axios instances, create an API gateway.
 export const axiosInstanceRooms = axios.create({
     baseURL: "http://localhost:8503/api",
 });
-
 axiosInstanceRooms.interceptors.request.use(
     config => {
         const token = localStorage.getItem('jwt');
@@ -36,8 +32,26 @@ axiosInstanceRooms.interceptors.request.use(
     },
     error => Promise.reject(error)
 );
-
 axiosInstanceRooms.interceptors.response.use(
+    response => response,
+    error => Promise.reject(error)
+);
+
+// TODO: Instead of two axios instances, create an API gateway.
+export const axiosInstanceReservations = axios.create({
+    baseURL: "http://localhost:8506/api",
+});
+axiosInstanceReservations.interceptors.request.use(
+    config => {
+        const token = localStorage.getItem('jwt');
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    error => Promise.reject(error)
+);
+axiosInstanceReservations.interceptors.response.use(
     response => response,
     error => Promise.reject(error)
 );
