8b6daaedee4c087ddfe9ba3501e409fcd9104822
magley <60552482+magley@users.noreply.github.com>
2025-08-31 17:03:06 +0200

    feat: Add initial API for the reservaition client

---
+++ src/api/reservation.api.ts
@@ -0,0 +1,61 @@
+import type { AxiosResponse } from "axios";
+import { axiosInstanceReservations } from "./util";
+
+export enum ReservationRequestStatus {
+    Pending = "pending",
+    Accepted = "accepted",
+    Rejected = "rejected",
+}
+
+export interface CreateReservationRequestDTO {
+    roomId: number;
+    dateFrom: string; // ISO string format
+    dateTo: string;
+    guestCount: number;
+}
+
+export interface ReservationRequestDTO {
+    id: number;
+    roomId: number;
+    dateFrom: string;
+    dateTo: string;
+    guestCount: number;
+    guestId: number;
+    status: ReservationRequestStatus;
+    cost: number;
+}
+
+export interface ReservationDTO {
+    id: number;
+    roomId: number;
+    dateFrom: string;
+    dateTo: string;
+    guestCount: number;
+    guestId: number;
+    cancelled: boolean;
+    cost: number;
+}
+
+export class ReservationAPI {
+    static createRequest(dto: CreateReservationRequestDTO): Promise<AxiosResponse<ReservationRequestDTO>> {
+        return axiosInstanceReservations.post("/req", dto);
+    }
+
+    static getRequestsByGuest(): Promise<AxiosResponse<ReservationRequestDTO[]>> {
+        return axiosInstanceReservations.get("/req/user");
+    }
+
+    static getRequestsByRoom(roomId: number): Promise<AxiosResponse<ReservationRequestDTO[]>> {
+        return axiosInstanceReservations.get(`/req/room/${roomId}`);
+    }
+
+    static deleteRequest(requestId: number): Promise<AxiosResponse<null>> {
+        return axiosInstanceReservations.delete(`/req/${requestId}`);
+    }
+
+    static checkAvailability(roomId: number, from: string, to: string): Promise<AxiosResponse<{ available: boolean }>> {
+        return axiosInstanceReservations.get(`/room/${roomId}/availability`, {
+            params: { from, to },
+        });
+    }
+}
