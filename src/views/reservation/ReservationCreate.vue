45b64e67979ba7d63f08afe70260686d695c8bf9
magley <60552482+magley@users.noreply.github.com>
2025-09-01 14:48:13 +0200

    feat: Show availability heatmap and redirect

--- src/views/reservation/ReservationCreate.vue
+++ src/views/reservation/ReservationCreate.vue
@@ -4,21 +4,57 @@ import { useRoute, useRouter } from 'vue-router';
 import { useAuthStore } from '../../stores/auth-store';
 import { ReservationAPI, ReservationRequestStatus, type CreateReservationRequestDTO, type ReservationRequestDTO } from '../../api/reservation.api';
 import type { AxiosError, AxiosResponse } from 'axios';
+import { type RoomAvailabilityListDTO, RoomAPI } from '../../api/room.api';
+import HeatmapCalendar from '../../components/HeatmapCalendar.vue';
 
 const route = useRoute();
 const router = useRouter();
 const auth = useAuthStore();
 const roomId = ref(-1);
+const roomAvailability = ref<RoomAvailabilityListDTO | null>(null);
 
 const formDateFrom = ref<Date>(new Date());
 const formDateTo = ref<Date>(new Date());
 const formGuestCount = ref(1);
 
+const loading = ref(false);
 const error = ref('');
 
 onMounted(() => { auth.checkLocalStorage(); });
-onMounted(() => { roomId.value = parseInt(route.params.id as string); });
+onMounted(() => {
+    roomId.value = parseInt(route.params.id as string);
+    loadRoomAvailability();
+});
 onMounted(() => { formDateTo.value.setDate(formDateFrom.value.getDate() + 7); });
+
+const loadRoomAvailability = () => {
+    loading.value = true;
+
+    RoomAPI.findCurrentAvailabilityListOfRoom(roomId.value)
+        .then((res: AxiosResponse<RoomAvailabilityListDTO>) => {
+            roomAvailability.value = res.data;
+            console.log(res.data);
+        })
+        .catch((err: AxiosError) => {
+            console.error(err);
+        })
+        .finally(() => {
+            loading.value = false;
+            ensureAvailabilityExists();
+        });
+};
+
+const ensureAvailabilityExists = () => {
+    if (roomAvailability.value === null) {
+        // TODO: Notify the user that the room is never available.
+        roomAvailability.value = {
+            id: 0,
+            effectiveFrom: new Date().toISOString(),
+            items: [],
+            roomId: roomId.value,
+        };
+    }
+}
 
 const onSubmitReservationRequest = () => {
     const dto: CreateReservationRequestDTO = {
@@ -28,13 +64,23 @@ const onSubmitReservationRequest = () => {
         guestCount: formGuestCount.value
     };
 
+    loading.value = true;
+
     ReservationAPI.createRequest(dto).then((res: AxiosResponse<ReservationRequestDTO>) => {
         const req = res.data;
-        console.log(req);
-
         error.value = '';
+        router.push(`/reservation/user/${auth.id}`);
     }).catch((err: AxiosError) => {
-        error.value = err.message;
+        console.log(err);
+        if (err.status == 409) {
+            error.value = "You already have a reservation booked for this room";
+        } else if (err.status == 400) {
+            error.value = "Invalid data, please make sure the room can be booked at this time";
+        } else {
+            error.value = err.message;
+        }
+    }).finally(() => {
+        loading.value = false;
     });
 }
 
@@ -54,33 +100,43 @@ const onFromDateChanged = () => {
     <h3>Create new reservation for room <RouterLink :to="`/room/${roomId}`">room {{ roomId }}</RouterLink>
     </h3>
 
-    <form @submit.prevent="onSubmitReservationRequest" class="flex flex-col gap-4">
-        <FloatLabel class="mt-small">
-            <label for="fromDate">From</label>
-            <DatePicker id="fromDate" v-model="formDateFrom" v-on:value-change="onFromDateChanged" date-format="dd MM"
-                showIcon class="w-full" />
-        </FloatLabel>
+    <div v-if="loading">
+        <ProgressBar mode="indeterminate" style="height: 6px"></ProgressBar>
+    </div>
+    <div>
 
-        <FloatLabel class="mt-small">
-            <label for="toDate">To</label>
-            <DatePicker id="toDate" v-model="formDateTo" :min-date="new Date(formDateFrom)" date-format="dd MM" showIcon
-                class="w-full" />
-        </FloatLabel>
+        <div v-if="roomAvailability !== null">
+            <HeatmapCalendar :availabilityItems="roomAvailability.items" :year="new Date().getFullYear()" />
+        </div>
 
-        <FloatLabel class="mt-small">
-            <InputNumber id="guestCount" v-model="formGuestCount" class="w-full" :min="1" :max="999" fluid />
-            <label for="basePrice">Number of guests</label>
-        </FloatLabel>
+        <form @submit.prevent="onSubmitReservationRequest" class="flex flex-col gap-4">
+            <FloatLabel class="mt-small">
+                <label for="fromDate">From</label>
+                <DatePicker id="fromDate" v-model="formDateFrom" v-on:value-change="onFromDateChanged"
+                    date-format="dd MM" showIcon class="w-full" />
+            </FloatLabel>
 
-        <Button type="submit">Submit a request</Button>
-        <br />
-        <small>The host will be notified about your request. You may delete it if you change your mind.</small>
+            <FloatLabel class="mt-small">
+                <label for="toDate">To</label>
+                <DatePicker id="toDate" v-model="formDateTo" :min-date="new Date(formDateFrom)" date-format="dd MM"
+                    showIcon class="w-full" />
+            </FloatLabel>
 
-        <Message v-show="error.length > 0" severity="error" size="small" variant="simple">
-            {{ error }}
-        </Message>
+            <FloatLabel class="mt-small">
+                <InputNumber id="guestCount" v-model="formGuestCount" class="w-full" :min="1" :max="999" fluid />
+                <label for="basePrice">Number of guests</label>
+            </FloatLabel>
 
-    </form>
+            <Button type="submit">Submit a request</Button>
+            <br />
+            <small>The host will be notified about your request. You may delete it if you change your mind.</small>
+
+            <Message v-show="error.length > 0" severity="error" size="small" variant="simple">
+                {{ error }}
+            </Message>
+
+        </form>
+    </div>
 </template>
 
 <style lang="css" scoped>
