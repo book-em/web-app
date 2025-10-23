940f83c524fb0de96156f50530b561f53686574c
Nada Zaric <nada0zaric@gmail.com>
2025-10-23 22:51:47 +0200

    fix: Add guestCancelCount field in ReservationRequestDTO

--- src/api/reservation.api.ts
+++ src/api/reservation.api.ts
@@ -23,6 +23,7 @@ export interface ReservationRequestDTO {
     guestId: number;
     status: ReservationRequestStatus;
     cost: number;
+    guestCancelCount: number;
 }
 
 export interface ReservationDTO {
