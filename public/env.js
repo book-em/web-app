7a3ec5f1cbb45a944b8e9bf7c4c14f72b085eabe
Hristina Adamovic <hristinacina@gmail.com>
2025-10-24 22:16:51 +0200

    feat: Add interceptor for notification service

--- public/env.js
+++ public/env.js
@@ -2,5 +2,6 @@ window.__ENV__ = {
     VITE_USER_SERVICE_URL: "http://localhost:8500",
     VITE_ROOM_SERVICE_URL: "http://localhost:8503",
     VITE_ROOM_SERVICE_IMAGES_URL: "http://localhost:8505",
-    VITE_RESERVATION_SERVICE_URL: "http://localhost:8506"
+    VITE_RESERVATION_SERVICE_URL: "http://localhost:8506",
+    VITE_NOTIFICATION_SERVICE_URL: "http://localhost:8508"
 };
